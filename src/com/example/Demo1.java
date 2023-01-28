92017501a37bf11779fb38fc3a1596b6bb46e22c
sahil <sahil@Sahil>
2023-01-28 14:54:58 +0530

    [Sahil] new changes in project

---
+++ src/com/example/Demo1.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Demo1 
+{
+
+  public static void main(String[] args) 
+  {
+	
+
+    System.out.println("Hello good morning");
+  
+  
+  
+  
+  
+  }
+
+}
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -17,4 +17,4 @@ public class Main
 //UN=Saichaudhari
 //PWD=chSai@13
 //URL=https://github.com/Saichaudhari/3sepA_github.git
-//token=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
+//token=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
